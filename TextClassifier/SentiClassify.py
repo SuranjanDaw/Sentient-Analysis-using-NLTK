ea07700ee29e3cd8280f6bb1de015867a83f058b
SuranjanDaw <suranjandaw@gmail.com>
2020-01-10 03:50:34 +0530

    used a new data set

--- TextClassifier/SentiClassify.py
+++ TextClassifier/SentiClassify.py
@@ -1,14 +1,12 @@
-from nltk.corpus import movie_reviews
-from nltk.corpus import stopwords
-from nltk.classify.scikitlearn import SklearnClassifier
 import nltk
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
 import random
-import pickle
-from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
-from sklearn.svm import SVC , LinearSVC , NuSVC
 from sklearn.linear_model import LogisticRegression, SGDClassifier
-from nltk.classify import ClassifierI
+from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
+from sklearn.svm import SVC, LinearSVC, NuSVC
 from statistics import mode
+from nltk import ClassifierI, SklearnClassifier
 
 class VotedClassifier(ClassifierI):
 
@@ -30,113 +28,71 @@ class VotedClassifier(ClassifierI):
 
         return (votes.count(mode(votes)) / len(votes))
 
+f = open("/home/suranjan/Documents/WebCrawler/NLP/TextClassifier/positive.txt", "r")
+pos = []
+pos = f.read().split("\n")
+f.close()
 
+f = open("/home/suranjan/Documents/WebCrawler/NLP/TextClassifier/negetive.txt", "r")
+neg = []
+neg = f.read().split("\n")
+f.close()
 
-document = []
-
-#list of all stopwords
-#st = stopwords.words("english")
-#print(st)
-
-
-#extracting the words from files and shuffling them
-for category in movie_reviews.categories():
-    for fileid in movie_reviews.fileids(category):
-        document.append((list(movie_reviews.words(fileid)) ,category))
-random.shuffle(document)
-
-count = 0
-count1 = 0
-all_words = []
-stop_word_removed = []
-
-#making a list of all the words and counting them
-for w in movie_reviews.words():
-    all_words.append(w.lower())
-    count = count +1
-print("Number of words in total: ",count)
-
-all_words_freq = nltk.FreqDist(all_words)
-'''print(all_words_freq.most_common(20))'''
-
-#making a list of all words except stop words and counting them and printing them
 '''
-for w in all_words:
-    if w not in st:
-        stop_word_removed.append(w.lower())
-        count1 = count1+1
-
-print("Number of words except the stop-words :",count1)
-
-stop_word_removed_freq = nltk.FreqDist(stop_word_removed)
-print(stop_word_removed_freq.most_common(20))
-print("plot over all words removed stop wors")
-stop_word_removed_freq.plot(30, cumulative = False)
+print("Positive :\n", pos[:10])
+print("Negetive :\n", neg[:10])
 '''
 
+sent = []
+for p in pos:
+    sent.append((p,"pos"))
+for n in neg:
+    sent.append((n,"neg"))
+#random.shuffle(sent)
+
+print("all:", sent[:100])
 
 
-#plotting the frequency distribution curve
-'''
-print("plot over all words")
-all_words_freq.plot(30, cumulative =False)
-'''
+word = []
+
+for p in pos:
+    word_list = word_tokenize(p)
+    for w in word_list:
+        word.append(w.lower())
+for n in neg:
+    word_list = word_tokenize(n)
+    for w in word_list:
+        word.append(w.lower())
+
+print("\n\n")
+word_list_freq = FreqDist(word)
+word_list_freq.plot(30)
 
 
-#word features
-word_feature = list(all_words_freq.keys())[:3000]
-#print(list(word_feature))
+word_features = list(word_list_freq.keys())[:5000]
+print(list(word_features))
 
-#this func is taking a list of words and checking if they are in 
-#word_feature and lableing them as Ture or False
-#this function always returns a vector of size of word_feature
-def find_feature(document):
-    features = {}
-    words =set(document)
-    for w in word_feature:
-        features[w] = (w in words)
-    
-    return features
+def getfeature(document):
+    feature = {}
+    words = word_tokenize(document)
+    for w in word_features:
+        feature[w] = w in words
 
-#sending a review from negetive set to find features and prinitng its result
+    return feature
 
-'''print(find_feature(movie_reviews.words("neg/cv000_29416.txt")))'''
+feature_set = [(getfeature(rev),category) for (rev,category) in sent]
 
-feature_set = [(find_feature(rev),category) for (rev,category) in document]
-
-
+random.shuffle(feature_set)
 
 
 #Naive Bayes Classifer
 
-train_set = feature_set[100:]
-test_set = feature_set[:100]
+train_set = feature_set[10000:]
+test_set = feature_set[:10000]
 
 #training the data set 
 bayeClassifier = nltk.NaiveBayesClassifier.train(train_set)
-
-#saving it in pickel
-'''
-bayeClassifierSave = open("NaiveBayesClassifier.pickle","wb")
-pickle.dump(bayeClassifier,bayeClassifierSave)
-bayeClassifierSave.close()
-'''
-
-# to load from pickle 
-'''
-bayeClassifier_file = open("NaiveBayesClassifier.pickle", "rb")
-bayeClassifier = pickle.load(bayeClassifier_file)
-bayeClassifier_file.close()
-'''
-
-
-
-#testing it on test set
-
 print("Original nltk bayes classifier accuracy: ",nltk.classify.accuracy(bayeClassifier, test_set))
-
-#To see the most top 15 words and ratio of their classification 
-'''bayeClassifier.show_most_informative_features(15)'''
 
 
 #trianing using sklearn library for MultiNomial Naive Bayes
@@ -194,3 +150,5 @@ print("Voted Accuracy: ", nltk.classify.accuracy(voted_classifier, test_set))
 
 for i in range(5):
     print("Sentiment for is =",voted_classifier.classify(test_set[i][0]),"with a confidence of = ",voted_classifier.confidence(test_set[i][0]))
+
+
